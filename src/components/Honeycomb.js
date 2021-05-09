155204790b50ea16a46c158236ec23385372376f
Aaron Smith <smitham50@gmail.com>
2021-05-09 09:17:28 -0400

    add honeycomb component

---
+++ src/components/Honeycomb.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const Honeycomb = () => {
+    return (
+        <div>
+            <ul id="grid" className="clear">
+                <li>
+                    <div className="hexagon hidden"></div>
+                </li>
+                <li>
+                    <div className="hexagon"></div>
+                </li>
+                <li>
+                    <div className="hexagon"></div>
+                </li>
+                <li>
+                    <div className="hexagon"></div>
+                </li>
+                <li>
+                    <div className="hexagon"></div>
+                </li>
+                <li>
+                    <div className="hexagon"></div>
+                </li>
+                <li>
+                    <div className="hexagon hidden"></div>
+                </li>
+                <li>
+                    <div className="hexagon"></div>
+                </li>
+                <li>
+                    <div className="hexagon"></div>
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+export default Honeycomb;
